2c19d2945711b077975113238b2bc8850e1ff2a8
jcy0627 <jcy0627@gmail.com>
2023-03-14 15:40:37 +0900

    qna

--- src/services/openai.ts
+++ src/services/openai.ts
@@ -13,7 +13,7 @@ if (!process.env.OPENAI_API_KEY) {
 }
 
 const configuration = new Configuration({
-  apiKey:  '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',
+  apiKey: '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',
 });
 export const openai = new OpenAIApi(configuration);
 
